be786626d2dc3001f205f07711faa98d7187861e
axeltaine <a.taine@simplon-charleville.fr>
2019-04-23 09:14:23 +0200

    incrémentation

--- index.php
+++ index.php
@@ -15,213 +15,227 @@
 <body>
 
 
-<?php include 'header.html'; ?>
-<div class="global">
+    <?php include 'header.html'; ?>
+    <div class="global">
 
-<!-- slider -->
+        <!-- slider -->
 
-    <div class="container-fluid">
-        <div class="row">
-            <div id="carouselSlides" class="carousel slide carousel-fade" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselSlides" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselSlides" data-slide-to="1"></li>
-                    <li data-target="#carouselSlides" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="http://zupimages.net/up/17/40/wq5z.jpg" class="d-block w-100" alt="Responsive image">
-                        <div class="carousel-caption d-md-block">
-                            <h1> CML <br>
-                                Pôle Formation</h1>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://zupimages.net/up/18/08/bdmu.jpg"  class=" d-block w-100"
-                            alt="Responsive image">
-                        <div class="carousel-caption  d-md-block ">
-                            <h1> CML <br>
-                                Pôle Formation</h1>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://cdn.radiofrance.fr/s3/cruiser-production/2016/09/4d7c4766-260a-418f-b08e-daa5df17c580/870x489_1_7.jpg" class=" d-block w-100"
-                            alt="Responsive image">
-                        <div class="carousel-caption  d-md-block">
-                            <h1> CML <br>
-                                Pôle Formation</h1>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselSlides" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselSlides" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-<!-- fin slider -->
-
-
-<!-- 360° -->
-
-    <div class="container">
-        <div class="row">
-
-            <iframe class="framebox inherit-locale " style="width: calc(100% + 40px); height: 347px;"
-                src="https://developers.google.com/vr/develop/web/vrview-web_3856854427e4d7472be1c6e08f96e052.frame?hl=fr"
-                id="autoSize-0">
-            </iframe>
-
-
-        </div>
-    </div>
-
-    <!-- fin 360° -->
-
-    <!-- parallax + cards -->
-
-    <div class="container-fluid">
-        <div class="parallax-window" data-parallax="scroll" data-image-src="img/campus-moulin-leblanc-1.jpg">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-xl-3 col-md-6 mb-4 mt-5 ">
-                    <div class="card border-left-primary shadow h-100 py-2 mx-auto">
-                        <div class="card-body ">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Formations
+                <div id="carouselSlides" class="carousel slide carousel-fade" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselSlides" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselSlides" data-slide-to="1"></li>
+                        <li data-target="#carouselSlides" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="http://zupimages.net/up/17/40/wq5z.jpg" class="d-block w-100"
+                                alt="Responsive image">
+                            <div class="carousel-caption d-md-block">
+                                <h1> CML <br>
+                                    Pôle Formation</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://zupimages.net/up/18/08/bdmu.jpg" class=" d-block w-100"
+                                alt="Responsive image">
+                            <div class="carousel-caption  d-md-block ">
+                                <h1> CML <br>
+                                    Pôle Formation</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.radiofrance.fr/s3/cruiser-production/2016/09/4d7c4766-260a-418f-b08e-daa5df17c580/870x489_1_7.jpg"
+                                class=" d-block w-100" alt="Responsive image">
+                            <div class="carousel-caption  d-md-block">
+                                <h1> CML <br>
+                                    Pôle Formation</h1>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselSlides" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselSlides" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- fin slider -->
+
+
+        <!-- 360° -->
+
+        <div class="container">
+            <div class="row">
+
+                <iframe class="framebox inherit-locale " style="width: calc(100% + 40px); height: 347px;"
+                    src="https://developers.google.com/vr/develop/web/vrview-web_3856854427e4d7472be1c6e08f96e052.frame?hl=fr"
+                    id="autoSize-0">
+                </iframe>
+
+
+            </div>
+        </div>
+
+        <!-- fin 360° -->
+
+        <!-- parallax + cards -->
+
+        <div class="container-fluid">
+            <div class="parallax-window" data-parallax="scroll" data-image-src="img/campus-moulin-leblanc-1.jpg">
+                <div class="row">
+                    <div class="col-xl-3 col-md-6 mb-4 mt-5 ">
+                        <div class="card border-left-primary shadow h-100 py-2 mx-auto">
+                            <div class="card-body ">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Formations
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">24</div>
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">24</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    <div class="col-auto">
+                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
 
-                <div class="col-xl-3 col-md-6 mb-4 mt-5">
-                    <div class="card border-left-primary shadow h-100 py-2 mx-auto">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Eleves
+                    <div class="col-xl-3 col-md-6 mb-4 mt-5">
+                        <div class="card border-left-primary shadow h-100 py-2 mx-auto">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Eleves
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">15.000</div>
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">15.000</div>
-                                </div>
-                                <div class="col-auto">
+                                    <div class="col-auto">
                                         <i class="fas fa-user-graduate fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
 
-                <div class="col-xl-3 col-md-6 mb-4 mt-5">
-                    <div class="card border-left-primary shadow h-100 py-2 mx-auto">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Visite
+                    <div class="col-xl-3 col-md-6 mb-4 mt-5">
+                        <div class="card border-left-primary shadow h-100 py-2 mx-auto">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Visite
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">9000</div>
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">9000</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    <div class="col-auto">
+                                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="col-xl-3 col-md-6 mb-4 mt-5">
-                    <div class="card border-left-primary shadow h-100 py-2 mx-auto">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Contact
+                    <div class="col-xl-3 col-md-6 mb-4 mt-5">
+                        <div class="card border-left-primary shadow h-100 py-2 mx-auto">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Contact
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                                     </div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    <div class="col-auto">
+                                        <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+
+
+        <!-- fin parallax + cards -->
+
+        <!-- text + img -->
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-6 col-md-6 ">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+
+
+                </div>
+                <div class="col-xl-6 col-md-6 ">
+                    <img src="img/campus-moulin-leblanc-coworking.jpg" style="max-width: 100%;">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12 col-md-12">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex
+                        illum
+                        tempore
+                        distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet.
+                        Iste.
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+
+        <!-- fin text + img -->
+
+        <!-- boutton top -->
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-3"></div>
+            <div class="col-lg-1"><a href="#"><i class="fas fa-chevron-up fa-2x" id="top"></i></a></div>
         </div>
     </div>
 
-
-    <!-- fin parallax + cards -->
-
-    <!-- text + img -->
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-6 col-md-6 ">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-
-
-            </div>
-            <div class="col-xl-6 col-md-6 ">
-                <img src="img/campus-moulin-leblanc-coworking.jpg" style="max-width: 100%;">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xl-12 col-md-12">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sequi corrupti fuga? Neque ex illum
-                    tempore
-                    distinctio, expedita nostrum nisi at dignissimos vel fuga veritatis quia. Tempore, ullam amet. Iste.
-                </p>
-
-            </div>
-        </div>
-
-    </div>
-
-     <!-- fin text + img -->
-
-      <!-- boutton top -->
-    <div class="row">
-      <div class="col-lg-4"></div>
-      <div class="col-lg-4"></div>
-      <div class="col-lg-3"></div>
-      <div class="col-lg-1"><a href="#"><i class="fas fa-chevron-up fa-2x" id="top"></i></a></div>
-     </div>
-  </div>
-
-  <!-- fin boutton top -->
+    <!-- fin boutton top -->
     <?php include 'footer.html'; ?>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
